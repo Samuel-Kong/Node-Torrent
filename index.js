e5facb4eb3b09eda7c4254d361092e2d3e77157d
Samuel-Kong <konglexinsamuel@gmail.com>
2025-05-26 13:53:33 +0800

    Step 3.2: Sending messageswith UDP

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
-
 const fs = require('fs');
 const bencode = require('bencode');
 
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-const trackerUrl = torrent.announce.toString('utf8');
+
+const url = urlParse(torrent.announce.toString('utf8'));
+
+
+const socket = dgram.createSocket('udp4');
+
+const myMsg = Buffer.from('hello?', 'utf8');
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+  console.log('message is', msg);
+});
